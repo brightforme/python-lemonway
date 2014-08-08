2834c1e24da6449e91fdf86cfd3dbdbe4c2f37d4
Rémy Turpin <rturpin@payplug.fr>
2014-08-08 11:33:45 +0200

    ADD - missed mapping file

---
+++ lemonway/mapping.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+lemonway_errors = {
+    'xx': {
+        '05': 'Autorisation refusée',
+        '14': 'Coordonnées bancaires ou cryptogramme visuel invalides',
+        '17': 'Annulation de l\'internaute',
+        '34': 'Suspicion de fraude',
+        '54': 'Date de validité de la carte dépassée',
+        '63': 'Règles de sécurité non respectées, transaction arretée',
+        '75': 'Porteur non authentifié 3-D Secure ou Nombre de tentatives de saisie du numéro',
+        '90': 'Service temporairement indisponible',
+        '99': 'Service temporairement indisponible'
+    },
+    'yy': {
+        '02': 'Erreur controle nombre d\'utilisations',
+        '03': 'Carte sur liste grise',
+        '05': 'Pays d\'émission de la carte inconnu',
+        '06': 'Pays d\'émission de la carte non autorisé',
+        '08': 'Pays d\'émission de la carte non autorisé',
+        '17': '3D error',
+        '99': 'Problème technique'
+    },
+    'zz': {
+        '04': 'Suspicion de fraude',
+        '05': 'Ne pas honorer',
+        '07': 'Suspicion de fraude',
+        '12': 'Transaction invalide',
+        '13': 'Montant invalide',
+        '14': 'Numéro de porteur invalide',
+        '15': 'Emetteur de carte inconnu',
+        '30': 'Erreur de format',
+        '33': 'Date de validité de la carte dépassée',
+        '34': 'Suspicion de fraude',
+        '41': 'Carte perdue',
+        '43': 'Carte volée',
+        '51': 'Provision insuffisante ou crédit dépassé',
+        '54': 'Date de validité de la carte dépassée',
+        '56': 'Carte absente du fichier',
+        '57': 'Transaction non permise à ce porteur',
+        '59': 'Suspicion de fraude',
+        '61': 'Dépasse la limite du montant de retrait',
+        '63': 'Règles de sécurité non respectées',
+        '68': 'Réponse non parvenue ou reçue trop tard',
+        '90': 'Arret momentané du système',
+        '91': 'Emetteur de cartes inaccessible',
+        '96': 'Mauvais fonctionnement du système',
+        '97': 'Echéance de la temporisation de surveillance globale',
+        '98': 'Serveur indisponible routage réseau demandé à nouveau',
+        '99': 'Incident domaine initiateur'
+    },
+    'dd': {
+        '01': 'Porteur non enrolé 3-D Secure',
+        '02': 'Problème technique sur l\'ACS',
+        '10': 'Impossible de déterminer si le porteur est enrolé ou non',
+        '55': 'Porteur non authentifié',
+        '81': 'Erreur interne',
+        '85': 'Serveur injoignable',
+        '94': 'Erreur technique au cours de l\'authentification sur le DS',
+        '95': 'Erreur d\intégrité sur le message renvoyé par l\ACS',
+        '96': 'Message renvoyé par l\'ACS invalide',
+        '97': 'Paramètres transmis invalides',
+        '98': 'Problème réseau lors de la tentative d\'accès aux DS',
+        '99': 'Erreur technique'
+    }
+}
