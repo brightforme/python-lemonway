ba41dc9bff21558d1712fe06751f867806d8abd6
Remy Turpin <rturpin@payplug.com>
2014-10-28 17:50:30 +0100

    ADD - newversion of python_lemonway with improvements of MoneyIn

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='python_lemonway',
-    version='0.1.0',
+    version='0.1.1',
     author='Pierre Pigeau',
     author_email='ppigeau@payplug.fr',
     packages=['lemonway'],
