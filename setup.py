fc1bce9560630b248d454ca67d16e704429c2f81
Pierre Pigeau <pierre.pigeau@gmail.com>
2015-06-19 09:53:28 +0200

    Add version V0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='python_lemonway',
-    version='0.2',
+    version='0.3',
     author='Pierre Pigeau',
     author_email='ppigeau@payplug.com',
     packages=['lemonway'],
